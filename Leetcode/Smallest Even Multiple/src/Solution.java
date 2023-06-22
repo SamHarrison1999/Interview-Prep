e17ac0144e7c0999481e1ba8cdc0524a93f31084
SamHarrison1999 <87808150+SamHarrison1999@users.noreply.github.com>
2023-06-22 19:40:35 +0100

    Smallest Even Multiple Java Solution

--- Leetcode/Smallest Even Multiple/src/Solution.java
+++ Leetcode/Smallest Even Multiple/src/Solution.java
@@ -1,6 +1,11 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
+        return n % 2 == 0 ? n : n * 2;
+        /*
         if (n % 2 == 0) return n;
         return 2 * n;
+         */
     }
+
+
 }
